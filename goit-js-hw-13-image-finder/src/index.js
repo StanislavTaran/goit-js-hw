475cdeff585d26ede85a4b7a946d31344aa316f0
Stas Taran <stastaransa1nt@gmail.com>
2020-02-25 22:22:03 +0200

    added hw13 v1

--- goit-js-hw-13-image-finder/src/index.js
+++ goit-js-hw-13-image-finder/src/index.js
@@ -1,29 +1,22 @@
 import './styles.css';
-import './services/getImages';
 import TempalateResultItem from './templates/result-item.hbs';
 import PNotify from '../node_modules/pnotify/dist/es/PNotify';
 import '../node_modules/pnotify/dist/PNotifyBrightTheme.css';
-// import getImages from './services/getImages';
 import InfiniteScroll from 'infinite-scroll';
 
 const refs = {
   input: document.getElementById('input'),
-  output: document.getElementById('gallery'),
   resultSection: document.getElementById('result-of-search'),
   searchForm: document.getElementById('search-form'),
-  searchList: document.getElementById('search-list'),
   searchButton: document.getElementById('search-btn'),
-  loadMoreButton: document.getElementById('load-more-btn'),
   galleryParent: document.getElementById('gallery'),
   lightbox: document.querySelector('.lightbox'),
   lightboxImage: document.querySelector('.lightbox__image'),
   lightboxOverlay: document.querySelector('.lightbox__content'),
-  sentinelObserve: document.getElementById('sentinel'),
-  debounce: require('lodash/debounce'),
 };
 
 const queryOptions = {
-  numberOfPage: 1,
+  proxyUrl: 'https://cors-anywhere.herokuapp.com/',
   searchValue: '',
   baseUrl:
     'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=',
@@ -50,11 +43,48 @@ const fetchQuery = {
   },
 };
 
+function handleSearchQuery(e) {
+  e.preventDefault();
+  const query = refs.input.value;
+  if (query !== queryOptions.searchValue) {
+    refs.galleryParent.innerHTML = '';
+    fetchQuery.getImages(query, queryOptions.numberOfPage).then(data => {
+      if (data.hits.length < 1) {
+        PNotify.alert({
+          text: 'Ничего не найдено по Вашему запросу.',
+          delay: 1000,
+        });
+      } else createMarkupResult(data);
+    });
+  } else if (query === '') {
+    PNotify.info({
+      text: 'Укажите, что нужно найти пожалуйста.',
+      delay: 1000,
+    });
+  }
+  queryOptions.searchValue = query;
+}
+
+function createMarkupResult(obj) {
+  console.log(obj.hits);
+  const result = obj.hits;
+  const markup = result
+    .map(item => {
+      return TempalateResultItem(item);
+    })
+    .join('');
+  drawMarkup(markup, refs.galleryParent);
+}
+
+function drawMarkup(markup, output) {
+  output.insertAdjacentHTML('beforeend', markup);
+}
+
 const lightBox = {
   openLightbox(event) {
-    event.preventDefault();
-
     const url = event.target.dataset.source;
+
+    if (event.target === refs.galleryParent) return;
 
     refs.lightbox.classList.add('is-open');
     refs.lightboxImage.setAttribute('src', url);
@@ -86,68 +116,13 @@ const lightBox = {
   },
 };
 
-function handleSearchQuery(e) {
-  e.preventDefault();
-  const query = refs.input.value;
-  if (query !== queryOptions.searchValue) {
-    refs.output.innerHTML = '';
-    fetchQuery.getImages(query, queryOptions.numberOfPage).then(data => {
-      if (data.hits.length < 1) {
-        PNotify.alert({
-          text: 'Ничего не найдено по Вашему запросу.',
-          delay: 1000,
-        });
-      } else createMarkupResult(data);
-    });
-  } else if (query === '') {
-    PNotify.info({
-      text: 'Укажите, что нужно найти пожалуйста.',
-      delay: 1000,
-    });
-  }
-  queryOptions.searchValue = query;
-}
-
-function createMarkupResult(obj) {
-  console.log(obj.hits);
-  const result = obj.hits;
-  const markup = result
-    .map(item => {
-      return TempalateResultItem(item);
-    })
-    .join('');
-  drawMarkup(markup, refs.output);
-}
-
-function drawMarkup(markup, output) {
-  output.insertAdjacentHTML('beforeend', markup);
-}
-
-function downloadNextPage() {
-  const query = refs.input.value;
-  queryOptions.numberOfPage++;
-  getImages.getImages(query, queryOptions.numberOfPage).then(data => {
-    createMarkupResult(data);
-  });
-}
-
-function clearSearchQuery() {
-  queryOptions.numberOfPage = 1;
-}
-
 const infScr = new InfiniteScroll(refs.galleryParent, {
   path() {
-    return (
-      queryOptions.baseUrl +
-      '&key=' +
-      queryOptions.API_KEY +
-      `&q=${query}` +
-      `&page=${this.pageIndex}`
-    );
+    return `${queryOptions.proxyUrl}${queryOptions.baseUrl}&per_page=12&key=${queryOptions.API_KEY}&q=${queryOptions.searchValue}&page=${this.pageIndex}`;
   },
   history: false,
   responseType: 'text',
-  status: '.page-load-status',
+  scrollThreshold: 1200,
 });
 
 infScr.on(`load`, response => {
@@ -156,8 +131,8 @@ infScr.on(`load`, response => {
   createMarkupResult(images);
 });
 
+// infScroll.on( 'request', function( path ) {...})
+
 refs.searchButton.addEventListener('click', handleSearchQuery);
-refs.input.addEventListener('input', clearSearchQuery);
-// refs.loadMoreButton.addEventListener('click', downloadNextPage);
 refs.galleryParent.addEventListener('click', lightBox.openLightbox);
 refs.lightboxOverlay.addEventListener('click', lightBox.handleOverlayClick);
